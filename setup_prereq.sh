638baec7fb126d07038011d2c843b7590e13c590
EC2 Default User <ec2-user@ip-172-31-50-44.us-west-2.compute.internal>
2021-08-20 17:48:02 +0000

    setup

---
+++ setup_prereq.sh
@@ -0,0 +1,12 @@
+wget https://services.gradle.org/distributions/gradle-7.1.1-bin.zip -P /tmp
+sudo unzip -d /opt/gradle /tmp/gradle-*.zip
+export PATH=$PATH:/opt/gradle/gradle-7.1.1/bin
+source ~/.bashrc
+
+# mkdir gradle
+# cd gradle
+# wget https://services.gradle.org/distributions/gradle-7.1.1-bin.zip
+# unzip gradle-7.1.1-bin.zipsam 
+# export PATH=$PATH:~/environment/amazon-automated-forecast/gradle/gradle-7.1.1/bin
+
+# source ~/.bashrc
